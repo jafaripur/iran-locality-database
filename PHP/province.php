49c9ce0a481d69aaa069277a96dfa41fbe74a10b
Mohsen Safari <mohsen.4887@gmail.com>
2019-12-20 19:22:56 +0330

    add php format

---
+++ PHP/province.php
@@ -0,0 +1,222 @@
+<?php
+
+$provinces = [
+    [
+        "id" => 1,
+        "country" => 1,
+        "name" => "آذربایجان شرقی",
+        "latitude" => 37.9035733,
+        "longitude" => 46.2682109
+    ],
+    [
+        "id" => 2,
+        "country" => 1,
+        "name" => "آذربایجان غربی",
+        "latitude" => 37.4550062,
+        "longitude" => 45
+    ],
+    [
+        "id" => 3,
+        "country" => 1,
+        "name" => "اردبیل",
+        "latitude" => 38.4853276,
+        "longitude" => 47.8911209
+    ],
+    [
+        "id" => 4,
+        "country" => 1,
+        "name" => "اصفهان",
+        "latitude" => 32.6546275,
+        "longitude" => 51.6679826
+    ],
+    [
+        "id" => 5,
+        "country" => 1,
+        "name" => "البرز",
+        "latitude" => 35.9960467,
+        "longitude" => 50.9289246
+    ],
+    [
+        "id" => 6,
+        "country" => 1,
+        "name" => "ایلام",
+        "latitude" => 33.2957618,
+        "longitude" => 46.670534
+    ],
+    [
+        "id" => 7,
+        "country" => 1,
+        "name" => "بوشهر",
+        "latitude" => 28.9233837,
+        "longitude" => 50.820314
+    ],
+    [
+        "id" => 8,
+        "country" => 1,
+        "name" => "تهران",
+        "latitude" => 35.696111,
+        "longitude" => 51.423056
+    ],
+    [
+        "id" => 9,
+        "country" => 1,
+        "name" => "چهارمحال و بختیاری",
+        "latitude" => 31.9614348,
+        "longitude" => 50.8456323
+    ],
+    [
+        "id" => 10,
+        "country" => 1,
+        "name" => "خراسان جنوبی",
+        "latitude" => 32.5175643,
+        "longitude" => 59.1041758
+    ],
+    [
+        "id" => 11,
+        "country" => 1,
+        "name" => "خراسان رضوی",
+        "latitude" => 35.1020253,
+        "longitude" => 59.1041758
+    ],
+    [
+        "id" => 12,
+        "country" => 1,
+        "name" => "خراسان شمالی",
+        "latitude" => 37.4710353,
+        "longitude" => 57.1013188
+    ],
+    [
+        "id" => 13,
+        "country" => 1,
+        "name" => "خوزستان",
+        "latitude" => 31.4360149,
+        "longitude" => 49.041312
+    ],
+    [
+        "id" => 14,
+        "country" => 1,
+        "name" => "زنجان",
+        "latitude" => 36.5018185,
+        "longitude" => 48.3988186
+    ],
+    [
+        "id" => 15,
+        "country" => 1,
+        "name" => "سمنان",
+        "latitude" => 35.2255585,
+        "longitude" => 54.4342138
+    ],
+    [
+        "id" => 16,
+        "country" => 1,
+        "name" => "سیستان و بلوچستان",
+        "latitude" => 27.5299906,
+        "longitude" => 60.5820676
+    ],
+    [
+        "id" => 17,
+        "country" => 1,
+        "name" => "فارس",
+        "latitude" => 29.1043813,
+        "longitude" => 53.045893
+    ],
+    [
+        "id" => 18,
+        "country" => 1,
+        "name" => "قزوین",
+        "latitude" => 36.0881317,
+        "longitude" => 49.8547266
+    ],
+    [
+        "id" => 19,
+        "country" => 1,
+        "name" => "قم",
+        "latitude" => 34.6399443,
+        "longitude" => 50.8759419
+    ],
+    [
+        "id" => 20,
+        "country" => 1,
+        "name" => "كردستان",
+        "latitude" => 35.9553579,
+        "longitude" => 47.1362125
+    ],
+    [
+        "id" => 21,
+        "country" => 1,
+        "name" => "كرمان",
+        "latitude" => 30.2839379,
+        "longitude" => 57.0833628
+    ],
+    [
+        "id" => 22,
+        "country" => 1,
+        "name" => "كرمانشاه",
+        "latitude" => 34.314167,
+        "longitude" => 47.065
+    ],
+    [
+        "id" => 23,
+        "country" => 1,
+        "name" => "کهگیلویه و بویراحمد",
+        "latitude" => 30.6509479,
+        "longitude" => 51.60525
+    ],
+    [
+        "id" => 24,
+        "country" => 1,
+        "name" => "گلستان",
+        "latitude" => 37.2898123,
+        "longitude" => 55.1375834
+    ],
+    [
+        "id" => 25,
+        "country" => 1,
+        "name" => "گیلان",
+        "latitude" => 37.1171617,
+        "longitude" => 49.5279996
+    ],
+    [
+        "id" => 26,
+        "country" => 1,
+        "name" => "لرستان",
+        "latitude" => 33.5818394,
+        "longitude" => 48.3988186
+    ],
+    [
+        "id" => 27,
+        "country" => 1,
+        "name" => "مازندران",
+        "latitude" => 36.2262393,
+        "longitude" => 52.5318604
+    ],
+    [
+        "id" => 28,
+        "country" => 1,
+        "name" => "مركزی",
+        "latitude" => 33.5093294,
+        "longitude" => -92.396119
+    ],
+    [
+        "id" => 29,
+        "country" => 1,
+        "name" => "هرمزگان",
+        "latitude" => 27.138723,
+        "longitude" => 55.1375834
+    ],
+    [
+        "id" => 30,
+        "country" => 1,
+        "name" => "همدان",
+        "latitude" => 34.7607999,
+        "longitude" => 48.3988186
+    ],
+    [
+        "id" => 31,
+        "country" => 1,
+        "name" => "یزد",
+        "latitude" => 32.1006387,
+        "longitude" => 54.4342138
+    ]
+];
+
